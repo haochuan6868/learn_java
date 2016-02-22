9cda7655e83bab5fab46cb24d24b28826c0f6838
haochuan6868 <haochuan6868@qq.com>
2016-02-22 16:28:40 +0800

    java array test

---
+++ test/src/testArray/arrayLength.java
@@ -0,0 +1,10 @@
+package testArray;
+
+public class arrayLength {
+	public static void main(String args[])
+	{
+		String[][] data = new String[2][5];
+		System.out.println("第一维数组长度:" + data.length);
+		System.out.println("第二维数组长度:" + data[0].length);
+	}
+}
