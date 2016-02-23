607010509b147ed6ef6701532cccae4dff7a8bba
haochuan <haochuan6868@qq.com>
2016-02-23 14:15:54 +0800

    test

---
+++ test/src/testTime/FormatTime.java
@@ -0,0 +1,14 @@
+package testTime;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class FormatTime {
+	public static void main(String[] args)
+	{
+		Date date = new Date();
+		String strDateFormat = "yyyy-MM-dd HH:mm:ss";
+		SimpleDateFormat sdf = new SimpleDateFormat(strDateFormat);
+		System.out.println(sdf.format(date));
+	}
+}
